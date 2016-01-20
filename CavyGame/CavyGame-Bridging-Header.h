93bf587caed8c744dad3cfd9c09a9291a5be8e82
bay2 <382499488@qq.com>
2016-01-20 14:44:09 +0800

    修改圆角像素点问题

--- CavyGame/CavyGame-Bridging-Header.h
+++ CavyGame/CavyGame-Bridging-Header.h
@@ -20,4 +20,5 @@
 #import "UIImageView+ProgressView.h"
 #import <TestinAgent/TestinAgent.h>
 #import "APService.h"
+#import "MJRefresh.h"
 #import "UITableView+FDTemplateLayoutCell.h"
--- Podfile
+++ Podfile
@@ -1,3 +1,4 @@
 platform :ios, "7.0"
-
-pod "MJRefresh"
+use_frameworks!
+c
+pod 'MJRefresh', '~> 3.0.8'
